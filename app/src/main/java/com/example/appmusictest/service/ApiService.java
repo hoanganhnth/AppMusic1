bfefad40cfebb77449cafd69848e1a386a7a2f3b
dnavnu534 <21020534@vnu.edu.vn>
2023-11-13 18:50:59 +0700

    Update URL Api

--- app/src/main/java/com/example/appmusictest/service/ApiService.java
+++ app/src/main/java/com/example/appmusictest/service/ApiService.java
@@ -2,7 +2,7 @@ package com.example.appmusictest.service;
 
 public class ApiService {
 //    private static String base_url = "https://hoanganhnth.000webhostapp.com/server/";
-    private static final String base_url = "https://hoanganhnth2k3.000webhostapp.com/musicApp/server/";
+    private static final String base_url = "http://34.124.226.151:3000";
     public static DataService getService() {
         return APIRetrofitClient.getClient(base_url).create(DataService.class);
     }
